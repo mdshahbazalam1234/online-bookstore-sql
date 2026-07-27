14ab9a41658d1dc0b4a2f9ae94a1cc03983747ae
mdshahbazalam1234 <mdalam805122@gmail.com>
2026-07-28 00:52:03 +0530

    Refactor SQL schema and queries for online bookstore

--- online_bookstore.sql.sql
+++ online_bookstore.sql.sql
@@ -107,33 +107,3 @@ LIMIT 1;
 SELECT sum(total_amount) 
 as total_revenue 
 FROM orders ;
-
--- Advanced Questions :
-
--- Q1. Retrieve the total number of books sold for each genre.
-
-SELECT b.genre, SUM(o.quantity) as total_books_sold
-FROM book as b 
-JOIN orders as o
-on b.book_id = o.book_id
-GROUP BY b.genre;
-
-
--- Q2. Find the average price of books in the "Fantasy" genre.
-
-SELECT AVG(price) AS average_price
-FROM book
-WHERE genre = 'Fantasy';
-
-
-
--- Q3. List customers who have placed at least 2 orders.
-
-SELECT c.name, count(o.order_id) as total_orders 
-FROM customers as c
-join orders as o
-on c.customer_id = o.customer_id 
-GROUP BY c.customer_id, o.order_id
-HAVING COUNT(o.order_id) >= 2
-;
-
